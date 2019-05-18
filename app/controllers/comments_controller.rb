b136c20f8c537400b92be34bc6d4f051ebb844f0
blackvd <aristideehouman@yahoo.fr>
2019-05-18 10:51:43 +0000

    Gestion des annonces et des commentaires partie 3

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,17 @@
 class CommentsController < ApplicationController
 
   def create
-    @advertisement = Advertisement.find(params[:find])
-    
+    @advertisement = Advertisement.find(params[:advertisement_id])
+    @comment = @advertisement.comments.new(comment_params)
+    if @comment.save
+      redirect_to advertisement_path(@advertisement)
+    end
   end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 
 end
